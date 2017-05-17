25edd5912fe9a4d6d8e3dca966cb269e6da3994d
BHARAT VERMA <bharat.verma@mobikwik.com>
2017-05-17 20:54:48 +0530

    Added get products API

--- commons/config/config.dev.php
+++ commons/config/config.dev.php
@@ -1,7 +1,7 @@
 <?php
 
-define($MYSQL_HOST, "172.17.0.2");
-define($MYSQL_PORT, "3306");
-define($MYSQL_USER, "root");
-define($MYSQL_PASS, "root");
-define($MYSQL_DB, "product");
+define('MYSQL_HOST', "172.17.0.2");
+define('MYSQL_PORT', "3306");
+define('MYSQL_USER', "root");
+define('MYSQL_PASS', "root");
+define('MYSQL_DB', "product");
